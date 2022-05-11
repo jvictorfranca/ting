9878a8a2f95da32cc316e21821969dcd733fcc9b
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2022-05-11 13:17:36 -0300

    faz o req 1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,23 @@
+
 class Queue:
+    queueList = []
     def __init__(self):
         """Inicialize sua estrutura aqui"""
+        self.queueList = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.queueList)
 
     def enqueue(self, value):
         """Aqui irá sua implementação"""
+        self.queueList.append(value)
 
     def dequeue(self):
         """Aqui irá sua implementação"""
+        return self.queueList.pop(0)
 
     def search(self, index):
+        if index < 0 or index > len(self.queueList):
+            raise IndexError()
+        return self.queueList[index]
         """Aqui irá sua implementação"""
